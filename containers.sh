4b033d8124ffb7420c4ad16f0d176957bf3dcf60
Chipo Manda <1mandachipo@gmail.com>
2025-11-10 10:34:48 +0200

    Revise comments in containers.sh
    Updated comments to clarify the use of Singularity containers.

--- containers.sh
+++ containers.sh
@@ -1,11 +1,8 @@
-# Container Dependencies
-
-This pipeline uses 3 Singularity containers from BioContainers:
+# This pipeline uses 3 Singularity containers from BioContainers:
 
 ### Download containers:
 
 ```bash
-# Download all containers
 singularity pull oras://community.wave.seqera.io/library/htseq_subread_trim-galore:aeb6b8b7800db0b0
 singularity pull oras://community.wave.seqera.io/library/bowtie2_fastp_samtools_star_pruned:5f151da513ade4ad
 singularity pull oras://community.wave.seqera.io/library/multiqc:1.21--d44678e7b9933bf6
