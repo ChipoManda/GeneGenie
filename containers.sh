530e6950947d1a56a19c605ee131666df8cfbea8
Chipo Manda <1mandachipo@gmail.com>
2025-11-10 11:19:32 +0000

    1

--- containers.sh
+++
@@ -1,9 +0,0 @@
-# This pipeline uses 3 Singularity containers from BioContainers:
-
-### Download containers:
-
-```bash
-singularity pull oras://community.wave.seqera.io/library/htseq_subread_trim-galore:aeb6b8b7800db0b0
-singularity pull oras://community.wave.seqera.io/library/bowtie2_fastp_samtools_star_pruned:5f151da513ade4ad
-singularity pull oras://community.wave.seqera.io/library/multiqc:1.21--d44678e7b9933bf6
-```
