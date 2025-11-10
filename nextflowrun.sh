2ad796f092e3b5c36f3133586b1ac60105230f78
Chipo Manda <1mandachipo@gmail.com>
2025-11-10 15:33:38 +0200

    Update nextflowrun.sh with new input parameters

--- nextflowrun.sh
+++ nextflowrun.sh
@@ -1,9 +1,9 @@
-nextflow run genegenie.nf -profile TSF
---input /home/mandac/Documents/gene_genie/data/mtb_cut/samplesheet_h37rv_cut.csv \
---outdir /home/mandac/Downloads/test/GeneGenie-1.0/output \
---gtf /home/mandac/Downloads/test/GeneGenie-1.0/genome/h37rv/GCF_000195955.2_ASM19595v2_genomic.gff \
---genome_fasta /home/mandac/Downloads/test/GeneGenie-1.0/genome/h37rv/GCF_000195955.2_ASM19595v2_cds_from_genomic.fna \
+nextflow run genegenie.nf -profile TSF \
+--input /path/to/reference/samplesheet.csv \
+--outdir /path/to/output \
+--gtf /path/to/reference/genomic.gtf \
+--genome_fasta /path/to/reference/genomic.fna \
 --max_cpus 8 \
---max_memory 24.GB \
---max_time 48.h 
--profile TBF
+--max_memory 16.GB \
+--max_time 48.h \
+--read_type single
